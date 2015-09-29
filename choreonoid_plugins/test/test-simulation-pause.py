4eebb78ec9683942c9168d4989c50c6fddb33926
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2015-09-29 10:10:01 +0900

    add test(sample) code for ROS service calls

---
+++ choreonoid_plugins/test/test-simulation-pause.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import time
+import rospy
+from std_srvs.srv import Empty
+from gazebo_msgs.srv import SpawnModel
+from gazebo_msgs.srv import DeleteModel
+
+ns = '/AISTSimulator/'
+
+def pause():
+    rospy.wait_for_service(ns + 'pause_physics')
+    try:
+        pausesrv = rospy.ServiceProxy(ns + 'pause_physics', Empty)
+        pausesrv()
+    except rospy.ServiceException, e:
+        print "Service call failed %s" % e
+
+def unpause():
+    rospy.wait_for_service(ns + 'unpause_physics')
+    try:
+        unpausesrv = rospy.ServiceProxy(ns + 'unpause_physics', Empty)
+        unpausesrv()
+    except rospy.ServiceException, e:
+        print "Service call failed %s" % e
+
+while True:
+    print "pause"
+    pause()
+    time.sleep(5)
+    print "unpause"
+    unpause()
+    time.sleep(5)
